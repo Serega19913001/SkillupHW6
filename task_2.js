f2c428903ab92fe89630d411f9819df5efd6533a
Serega19913001 <serlevchenko@ukr.net>
2021-04-19 22:23:05 +0300

    commit

---
+++ task_2.js
@@ -0,0 +1,10 @@
+/* Задание 2
+Получить массив объектов пользователей по цвету глаз (поле eyeColor). */
+
+import users from "./users.js";
+
+  const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color).map(user => user.name );
+
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
